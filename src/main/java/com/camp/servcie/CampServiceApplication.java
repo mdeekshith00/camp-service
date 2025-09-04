ce2841ea711e0d44593a5fa4344b101b6c5e9db5
mdeekshith00 <mdeekshith000@gmail.com>
2025-09-04 23:36:34 +0530

    initial commit

---
+++ src/main/java/com/camp/servcie/CampServiceApplication.java
@@ -0,0 +1,13 @@
+package com.camp.servcie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/camp/servcie/CampServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camp.servcie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
